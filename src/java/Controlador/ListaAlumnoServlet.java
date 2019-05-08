9f0d3b6714dbed1e95fe5e03100851859bc6af19
Christian <christiancazu@gmail.com>
2019-05-08 20:14:45 +0100

    refactorizado controlador ListaAlumno, agregado boton Listar Alumno en index.html

--- src/java/Controlador/ListaAlumnoServlet.java
+++ src/java/Controlador/ListaAlumnoServlet.java
@@ -1,8 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Controlador;
 
 import Entidad.Alumno;
 import Modelo.ModelAlumno;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,22 +17,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ListaAlumnoServlet
+ *
+ * @author Christian Carrillo Zúñiga
  */
-@WebServlet("/listaAlumno")
+@WebServlet(name = "ListaAlumnoServlet", urlPatterns = {"/alumnos"})
 public class ListaAlumnoServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // 1 Se obtiene una arraylist con los alumnos de la base de datos
+        ModelAlumno modelAlumno = new ModelAlumno();
+        List<Alumno> alumnos = modelAlumno.listaAlumno();
 
-    protected void service(HttpServletRequest request, HttpServletResponse response) 
-                throws ServletException, IOException {
-        // 1 Se obtiene una arraylist con los//alumnos de la base de datos
-        ModelAlumno m = new ModelAlumno();
-        List<Alumno> data = m.listaAlumno();
         // 2 Se guarda el arraylist en request con el alias alumnos
         // El request es una memoria temporal
-        request.setAttribute("alumnos", data);
-        // 3 Se reenvia el request al formulario
+        request.setAttribute("alumnos", alumnos);
+
+        // 3 Se reenvia el request al formulario 
         request.getRequestDispatcher("/listaAlumno.jsp").forward(request, response);
     }
+
 }
