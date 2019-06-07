441c2e6a460fbe61aadd3316a7e808be2d35bd12
Christian <christiancazu@gmail.com>
2019-06-07 06:02:23 -0500

    optimizado: controlador ListarPelicula

--- src/java/controladores/ListarPeliculasControlador.java
+++ src/java/controladores/ListarPeliculasControlador.java
@@ -6,7 +6,6 @@ import dao.fabrica.DAOFabrica;
 import entidades.Encuesta;
 import entidades.Pelicula;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,7 +14,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelos.FullPelicula;
 import utils.GenerateFullPelicula;
 
 /**
@@ -25,6 +23,24 @@ import utils.GenerateFullPelicula;
 @WebServlet(name = "ListarPeliculasControlador", urlPatterns = {"/peliculas"})
 public class ListarPeliculasControlador extends HttpServlet {
 
+    private DAOFabrica subFabrica;
+    private IPeliculaDAO iPeliculaDAO;
+    private IEncuestaDAO iEncuestaDAO;
+
+    /**
+     * servlet objects dao's initialization
+     *
+     * @throws ServletException
+     */
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        subFabrica = DAOFabrica.getDAOFabrica(DAOFabrica.MYSQL);
+        iPeliculaDAO = subFabrica.getPeliculaDAO();
+        iEncuestaDAO = subFabrica.getEncuestaDAO();
+    }    
+    
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -37,22 +53,16 @@ public class ListarPeliculasControlador extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        DAOFabrica subFabrica = DAOFabrica.getDAOFabrica(DAOFabrica.MYSQL);
-        IPeliculaDAO iPeliculaDAO = subFabrica.getPeliculaDAO();
-        IEncuestaDAO iEncuestaDAO = subFabrica.getEncuestaDAO();
-
         try {
             List<Pelicula> peliculas = iPeliculaDAO.getAll();
             List<Encuesta> encuestas = iEncuestaDAO.getAll();
 
-            // assign data: Pelicula, likes, dislikes for each Pelicula
+            // assign data: Pelicula, likes, dislikes for each Pelicula in FullPelicula structure
             request.setAttribute("fullPeliculas", GenerateFullPelicula.assignFullDataToPeliculas(peliculas, encuestas));
         } catch (Exception ex) {
             Logger.getLogger(ListarPeliculasControlador.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         request.getRequestDispatcher("WEB-INF/pages/peliculas.jsp").forward(request, response);
-    }
-
-    
+    }    
 }
